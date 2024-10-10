09ca5e7e615e74fd76efe8482d08ef7239435e9e
Linh Hoang <linhhoang981221@gmail.com>
2024-10-10 20:46:28 +0700

    auth component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,25 @@ import {Button} from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-        <Button size="sm">Click me</Button>
+        <Button>Primary</Button>
+        <Button variant="secondary">
+            Secondary
+        </Button>
+        <Button variant="destructive">
+            Destructive
+        </Button>
+        <Button variant="ghost">
+            Ghost
+        </Button>
+        <Button variant="muted">
+            Muted
+        </Button>
+        <Button variant="outline">
+            Outline
+        </Button>
+        <Button variant="teritary">
+            Teritary
+        </Button>
     </div>
   );
 }
