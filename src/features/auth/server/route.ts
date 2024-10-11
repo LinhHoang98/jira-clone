3b524ed87487cb6cf247a5e70c9f8a4adc05e332
Linh Hoang <linhhoang981221@gmail.com>
2024-10-11 21:13:22 +0700

    login, register integrate with appwrite

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,6 +1,10 @@
 import {Hono} from "hono";
 import {zValidator} from "@hono/zod-validator";
 import {loginSchema, registerSchema} from "@/features/auth/schemas";
+import {createAdminClient} from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import {deleteCookie, setCookie} from "hono/cookie";
+import {AUTH_COOKIE} from "@/features/auth/constants";
 
 const app = new Hono()
     .post(
@@ -9,9 +13,20 @@ const app = new Hono()
         async (c) => {
             const {email, password} = c.req.valid("json");
 
-            console.log({email, password});
+            const { account } = await createAdminClient();
+            const session = await account.createEmailPasswordSession(
+                email,
+                password,
+            );
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
+            });
 
-            return c.json({email, password});
+            return c.json({ success: true });
         }
     )
     .post(
@@ -20,9 +35,26 @@ const app = new Hono()
         async (c) => {
             const {email, name, password} = c.req.valid("json");
 
-            console.log({email, name, password});
+            const { account } = await createAdminClient();
+            await account.create(
+                ID.unique(),
+                email,
+                password,
+                name,
+            );
+            const session = await account.createEmailPasswordSession(
+                email,
+                password,
+            );
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
+            });
 
-            return c.json({email, name, password});
+            return c.json({ success: true });
         }
     )
 ;
